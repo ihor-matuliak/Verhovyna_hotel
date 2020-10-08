78097b2f64937aca88fd2fb0930700264b6a9cd1
ihor_matuliak <ihor.matuliak.r@gmail.com>
2020-10-08 16:40:08 +0300

    add medical_page

--- wp-content/themes/verhovyna/js/main2.js
+++ wp-content/themes/verhovyna/js/main2.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
 
-
+    // adaptive block
     const serviceImg = document.querySelectorAll('.service_img')
+    const chessBlockImg = document.querySelectorAll('.block_img')
+    const medicalProgramBlock = document.querySelectorAll('.program_block')
 
     adaptiveBlock = (nameBlock, relative) => {
         for(let i=0; i<nameBlock.length; i++){
@@ -12,6 +14,6 @@ $(document).ready(function () {
     }
 
     adaptiveBlock(serviceImg, 0.85);
-
-
+    adaptiveBlock(chessBlockImg, 0.75);
+    adaptiveBlock(medicalProgramBlock, 0.9);
 });
