ac85e242dcad800e2607252d42edbcae16f17c87
ihor_matuliak <ihor.matuliak.r@gmail.com>
2020-10-12 17:08:26 +0300

    add popups

--- wp-content/themes/verhovyna/js/main2.js
+++ wp-content/themes/verhovyna/js/main2.js
@@ -16,4 +16,37 @@ $(document).ready(function () {
     adaptiveBlock(serviceImg, 0.85);
     adaptiveBlock(chessBlockImg, 0.75);
     adaptiveBlock(medicalProgramBlock, 0.9);
+
+
+
+    // open popup
+    const body = document.querySelector('body');
+    function openPopup(btnClick, namePopup, addClassPopup){
+        const btn = document.querySelectorAll(btnClick);
+        const popup = document.querySelector(namePopup);
+
+        if(btn.length > 0){
+            for(let i=0; i<btn.length; i++){
+                btn[i].addEventListener('click', () =>{
+                    body.classList.add('hidden');
+                    popup.classList.add(addClassPopup);
+                })
+            }
+        }
+    }
+    openPopup('.open_popup', '#booking_popup', 'active');
+    
+
+    // close popup
+    const closeBtn = document.querySelectorAll('.close_popup');
+    if(closeBtn.length > 0){
+        for(let i=0; i<closeBtn.length; i++){
+            closeBtn[i].addEventListener('click', () =>{
+                body.classList.remove('hidden');
+                closeBtn[i].closest('.popup_main').classList.remove('active');
+            })
+        }
+    }
+
+    
 });
